755c5cf934397bee705fd4ae012b4ae778698145
sudo-bencho <182349246+sudo-bencho@users.noreply.github.com>
2025-09-14 12:49:11 +0000

    Update run-fetch.sh

--- assets/run-fetch.sh
+++ assets/run-fetch.sh
@@ -3,16 +3,16 @@
 
 /usr/bin/gobetween -c /etc/gobtw.toml &
 
-date +%s > /tmp/drain_127.0.0.1_10002
-
+#date +%s > /tmp/drain_127.0.0.1_10002
+echo 10001>/tmp/myport.fetch
 while (true); do 
-myport=10001
+#myport=10001
 
 #for m in 1 2 3 4 5;do 
 #test -e /tmp/drain_127.0.0.1_$myport && myport=$(($myport+1))
 #done
-
-test -e /tmp/drain_127.0.0.1_$myport || myport=10002
+myport=$(cat /tmp/myport.fetch)
+## test -e /tmp/drain_127.0.0.1_$myport || myport=10002
 [[ "$DEBUG"  == "true" ]] && echo "SWITCH_PORT: $myport"
 export PORT=$myport
 rm     /tmp/drain_127.0.0.1_$myport ;
@@ -20,5 +20,7 @@ timeout 330 /usr/bin/subfetch &
 sleep 270 ; 
 touch  /tmp/drain_127.0.0.1_$myport
 sleep 60
+[[ $myport -eq 10001 ]] && (echo 10002 > /tmp/myport.fetch  )
+[[ $myport -eq 10002 ]] && (echo 10001 > /tmp/myport.fetch  )
 
 done
