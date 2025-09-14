0c62fb071fb6fd0ba46c510d306aac67cfe0f09a
sudo-bencho <182349246+sudo-bencho@users.noreply.github.com>
2025-09-14 15:24:54 +0000

    Update run-fetch.sh

--- assets/run-fetch.sh
+++ assets/run-fetch.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 
-/usr/bin/gobetween -c /etc/gobtw.toml &
+( /usr/bin/gobetween -c /etc/gobtw.toml 2>&1 | grep -v "ending to scheduler" ) &
 
 #date +%s > /tmp/drain_127.0.0.1_10002
 echo 10001 > /tmp/myport.fetch
