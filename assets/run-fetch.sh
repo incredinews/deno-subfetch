c902c6fbe31e1f5aad83cac9253c23e274ec532b
sudo-bencho <182349246+sudo-bencho@users.noreply.github.com>
2025-09-16 23:15:39 +0200

    Update run-fetch.sh

--- assets/run-fetch.sh
+++ assets/run-fetch.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-test -e /etc/connector.conf &&  ( /connector --config /etc/connector.conf  2>&1 |grep -v -e decryption -e key -e keepalive -e ndshake -e TUN -e Interface -e encryption ) &
+test -e /etc/connector.conf &&  ( echo "start conn..."; /connector --config /etc/connector.conf  2>&1 |grep -v -e decryp -e key -e keepalive -e ndshake -e TUN -e Interface -e encryp ) &
 ( /usr/bin/gobetween -c /etc/gobtw.toml 2>&1 | grep -v "ending to scheduler" ) &
 
 #date +%s > /tmp/drain_127.0.0.1_10002
